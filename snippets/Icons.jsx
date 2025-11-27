779a27153a14401df8a38b801af5389afff66000
Gabriel <gabrielraeder@outlook.com.br>
2025-11-27 12:09:33 -0300

    update icons

--- snippets/Icons.jsx
+++ snippets/Icons.jsx
@@ -98,3 +98,22 @@ export const CSharpIcon = (props) => (
     />
   </svg>
 );
+
+export const PipeIcon = (props) => (
+  <svg
+    style={{
+      display: "inline-block",
+      width: "1.5rem",
+      height: "1.5rem",
+    }}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      d="M22 14h-2v2h-6v-3h2v-2h-2V6a2 2 0 0 0-2-2H4V2H2v8h2V8h6v3H8v2h2v5a2 2 0 0 0 2 2h8v2h2"
+      style={{
+        fill: "#28a561",
+      }}
+    />
+  </svg>
+);
